becb48dd3ad1fcbb39e9fa9ed9d7e29ad2b6657b
EasyDev11436 <easy.dev11436@gmail.com>
2024-11-15 10:05:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PlayLog
+
+[![Visit Page](https://img.shields.io/badge/Visit_Page-Click_Here-brightgreen?style=for-the-badge)](https://playlogs.vercel.app)
+
+© EasyDeveloper 2024. All Rights Reserved.
+
+This software is the exclusive property of EasyDeveloper. 
+Unauthorized copying, modification, distribution, or use of this software, 
+in whole or in part, is strictly prohibited.
+
+Contact: easy.dev11436@gmail.com for any inquiries.
---
+++ app/layout.js
@@ -0,0 +1,29 @@
+// app/layout.js
+import './globals.css';
+
+export const metadata = {
+  title: 'PlayLog',
+  description: 'A simple app to manage your game list',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="bg-gray-100">
+        {children}
+      </body>
+    </html>
+  );
+}
